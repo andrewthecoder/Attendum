ec8ec207d7d7ede79c61964d4fc93f891cb36849
Will Pillar <me@willpillar.com>
2011-11-12 17:08:10 +0000

    Changed homepage HTML. (again)

--- application/views/home.php
+++ application/views/home.php
@@ -5,19 +5,22 @@
 		<div class="span10">
 			<h2>Welcome to Attendum</h2>
 			<p>Attendum is a new way to reward students for engaging with their course.</p>
-			<h3>Login</h3>
-			<form action="<?php echo site_url('user/login'); ?> method="post">
-			<table>
-			<tr>
-			<td><label for="username">Username</label></td><td><input type="text" class="span3" name="username"></td>
-			</tr>
-			<tr>
-			<td><label for="password">Password</label></td><td><input type="text" class="span3" name="password"></td>
-			</tr>
-			<tr>
-			<td></td><td><input type="submit" name="login" class="btn primary" value="Login"></td>
-			</tr>
-			</table>
+			<div id="login" class="span6">
+				<h3>Login</h3>
+				<form action="<?php echo site_url('user/login'); ?> method="post">
+					<table>
+					<tr>
+					<td><label for="username">Username</label></td><td><input type="text" name="username"></td>
+					</tr>
+					<tr>
+					<td><label for="password">Password</label></td><td><input type="text" name="password"></td>
+					</tr>
+					<tr>
+					<td></td><td><input type="submit" name="login" class="btn primary" value="Login"></td>
+					</tr>
+					</table>
+				</form>
+			</div>
 		</div>
 		<div class="span6">
 			<h3>Sign Up</h3>
