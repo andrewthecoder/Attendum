7b463e54084fcaed4cb6ef45a8f9fb442a638e53
Will Pillar <me@willpillar.com>
2011-11-12 14:35:11 +0000

    Signup changes.

--- application/views/home.php
+++ application/views/home.php
@@ -6,7 +6,7 @@ Attendum - Rewarding Your Attendance
 <link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css">
 </head>
 <body>
-<div id="container">
+<div class="container">
 <?php foreach($user1 as $user): ?>
 <p>
 Uid:<?php echo $user->uid; ?>
@@ -25,22 +25,23 @@ Description:<?php echo $a->description; ?>
 <?php endforeach; ?>
 	<div>
 		<h3>Sign Up</h3>
-		<?php echo validation_errors('<div class="alert-message warning"><a class="close" href="#">×</a><p>','</p></div>'); ?>
+		<?php echo validation_errors('<div class="alert-message error"><a class="close" href="#">×</a><p>','</p></div>'); ?>
 		<form action="<?php echo site_url('user/signup'); ?>" method="post">
 			<table>
 			<tr>
-			<td>Email</td>
-			<td><input type="text" name="email"></td>
+				<td>Email</td>
+				<td><input type="text" name="email"></td>
 			</tr>
 			<tr>
-			<td>Password</td>
-			<td><input type="password" name="pass"></td>
-			<td>Confirm Password</td>
-			<td><input type="password" name="passconf"></td>
+				<td>Password</td>
+				<td><input type="password" name="pass"></td>
+			</tr>
+				<td>Confirm Password</td>
+				<td><input type="password" name="passconf"></td>
 			</tr>
 			<tr>
-			<td><input type="submit" name="submit" value="Sign Up"></td>
-			<td></td>
+				<td><input type="submit" name="submit" value="Sign Up"></td>
+				<td></td>
 			</tr>
 			</table>
 		</form>
