d7be9d65ed4b749a2760c63c42f03e41b729df91
Andrew Beveridge <github@andrewbeveridge.co.uk>
2011-11-12 22:15:05 +0000

    test create code includes

--- application/views/create_code.php
+++ application/views/create_code.php
@@ -1,13 +1,4 @@
-<!doctype html>
-<head>
-	<title>
-	Create Code | Attendum
-	</title>
-	<link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css">
-	<link type="text/css" href="<?php echo base_url(); ?>css/ui-lightness/jquery-ui-1.8.16.custom.css" rel="Stylesheet" />	
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
-	<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery-ui-timepicker-addon.js"></script>
+<?php $this->load->view('inc/meta.php'); ?>
 	<script>
 		$(function() {
 			$( "#validfrom" ).datetimepicker({ 
@@ -35,9 +26,9 @@
 		.ui-timepicker-div dl dd{ margin: -25px 0 10px 65px; }
 		.ui-timepicker-div td { font-size: 90%; }
 	</style>
-</head>
-<body>
-	<div class="container">
+
+<div class="container">
+	<?php $this->load->view('inc/header.php'); ?>
 		<div style="padding-top: 30px;">
 			<h3>Create Code</h3>
 			<form action="<?php echo site_url('admin/submit_code'); ?>" method="post">
@@ -62,5 +53,4 @@
 			</form>
 		</div>
 	</div>
-</body>
-</html>
+<?php $this->load->view('inc/footer.php'); ?>
