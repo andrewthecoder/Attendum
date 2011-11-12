87fee7ddd51d020e4d83332a9ecb87b853e5ecc9
Harry George <monkeymanlives@gmail.com>
2011-11-12 18:50:56 +0000

    asd

--- application/views/about.php
+++ application/views/about.php
@@ -1,18 +1,16 @@
 <?php $this->load->view('inc/meta.php'); ?>
 <div class="container">
 	<?php $this->load->view('inc/header.php'); ?>
-	<div class="row">
-		<h2>About Us</h2>
-		<h3>Attendum</h3>
-		<p>Attendum is in two main sections.</p>
-		<p>The first is a fun game for students to gain achievements and even prizes for attending lectures. They can track their progress 
-			and compare with their friends and other classmates. </p>
-		<p>The second section of Attendum is the ability for the university to use this data to find out early on I there are any problematic 
-			areas where for some reason or other the attendance has greatly dropped for a certain module. If this happened the university would 
-			be given enough notice to investigate and hopefully solve the problem. </p>
-		<h3>Sudo Team</h3>
-		<p>We are a collection of computer science students with 10 years’ experience between us from 2 countries. We created this website in 
-		24 hours at the DevXS conference. </p>		
-	</div>
+	<h2>About Us</h2>
+	<h3>Attendum</h3>
+	<p>Attendum is in two main sections.</p>
+	<p>The first is a fun game for students to gain achievements and even prizes for attending lectures. They can track their progress 
+		and compare with their friends and other classmates. </p>
+	<p>The second section of Attendum is the ability for the university to use this data to find out early on I there are any problematic 
+		areas where for some reason or other the attendance has greatly dropped for a certain module. If this happened the university would 
+		be given enough notice to investigate and hopefully solve the problem. </p>
+	<h3>Sudo Team</h3>
+	<p>We are a collection of computer science students with 10 years’ experience between us from 2 countries. We created this website in 
+	24 hours at the DevXS conference. </p>		
 </div>
 <?php $this->load->view('inc/footer.php'); ?>
