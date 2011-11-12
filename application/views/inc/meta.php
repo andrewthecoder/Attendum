4f26a955dd09c84838302dd298ac1c2188b865f6
Will Pillar <me@willpillar.com>
2011-11-12 16:41:37 +0000

    Added stylesheets.

--- application/views/inc/meta.php
+++ application/views/inc/meta.php
@@ -4,5 +4,6 @@
 Attendum - Rewarding Your Attendance
 </title>
 <link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo base_url(); ?>css/style.css">
 </head>
 <body>
