0b568b46a1217018d08ec424181db47edc084a4b
Will Pillar <me@willpillar.com>
2011-11-12 20:55:25 +0000

    Fixed flashdata.

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -59,7 +59,7 @@ class User extends CI_Controller {
 				redirect('/');
 			}
 			else {
-				$this->session->flashdata('login-failure', 'Login Failed: Email/Password Incorrect');
+				$this->session->set_flashdata('login-failure', 'Login Failed: Email/Password Incorrect');
 				redirect('/');
 			}
 		}
