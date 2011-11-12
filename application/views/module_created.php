683767ba3083942a2ca81eeb008ecc37321d5ecf
Andrew Beveridge <github@andrewbeveridge.co.uk>
2011-11-12 17:34:38 +0000

    Add sarcasm

--- application/views/module_created.php
+++ application/views/module_created.php
@@ -6,8 +6,9 @@ Module Created | Attendum
 <link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css">
 </head>
 <body>
-	<div class="container">
+	<div class="container" style="padding-top: 30px;">
 	<h1 style="color:green;">Module Created!</h1>
+	<p>...Clever boy.</p>
 	</div>
 </body>
 </html>
