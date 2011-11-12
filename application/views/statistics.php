d135ca73b71cb308838e7cf06fa58f6be04ae644
Harry George <monkeymanlives@gmail.com>
2011-11-12 16:13:22 +0000

    asd

--- application/views/statistics.php
+++ application/views/statistics.php
@@ -10,6 +10,7 @@
 
 		// Set a callback to run when the Google Visualization API is loaded.
 		google.setOnLoadCallback(drawChart);
+		google.setOnLoadCallback(drawChart2);
 
 		// Callback that creates and populates a data table, 
 		// instantiates the pie chart, passes in the data and
@@ -35,24 +36,11 @@
 			// Instantiate and draw our chart, passing in some options.
 			var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
 			chart.draw(data, options);
-		}
-		
-	
-	</script>
-	<script type="text/javascript">
-
-		// Load the Visualization API and the piechart package.
-		google.load('visualization', '1.0', {'packages':['corechart']});
-
-		// Set a callback to run when the Google Visualization API is loaded.
-		google.setOnLoadCallback(drawChart);
-	function drawChart() {
-
-		// Create the data table.
-			var data = new google.visualization.DataTable();
-			data.addColumn('string', 'Module');
-			data.addColumn('number', 'Number Of Students');
-			data.addRows([
+			
+			var data1 = new google.visualization.DataTable();
+			data1.addColumn('string', 'Module');
+			data1.addColumn('number', 'Number Of Students');
+			data1.addRows([
 				<?php foreach($numofusersforcourse as $numofusersforcourse1):
 				echo "['$numofusersforcourse1->name', $numofusersforcourse1->num],
 				"; 
@@ -65,16 +53,18 @@
 			'height':600};
 
 			// Instantiate and draw our chart, passing in some options.
-			var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-			chart.draw(data, options);
+			var chart1 = new google.visualization.BarChart(document.getElementById('chart_div1'));
+			chart.draw(data1, options1);
 		}
-		</script>
+		
+	</script>
 </head>
 
 <body>
 <!--Div that will hold the pie chart-->
 <div id="container">
 <div id="chart_div"></div>
+<div id="chart_div1"></div>
 </div>
 </body>
 </html>
