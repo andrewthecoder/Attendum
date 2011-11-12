1fac418d64efa2a8fb6c8624d17bc9efeab3c278
Will Pillar <me@willpillar.com>
2011-11-12 10:15:30 +0000

    Added another echo message.

--- index.php
+++ index.php
@@ -2,12 +2,7 @@
 session_start();
 $page_title = "Home"; //This will be the page title
 include("header.ini");
-<<<<<<< HEAD
 echo "you suck harder!";
-
-=======
-asd
-echo "you suck harder blood!";
->>>>>>> 0ec572a842c1561225297b92d6b34e267b3d94fa
+echo "Sudo Team";
 include("footer.ini");
 ?>
