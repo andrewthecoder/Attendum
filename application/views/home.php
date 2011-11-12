ddfc89786d833a17779b2f5d47e9eaad8295254e
Will Pillar <me@willpillar.com>
2011-11-12 17:03:10 +0000

    Homepage html, styling.

--- application/views/home.php
+++ application/views/home.php
@@ -3,8 +3,14 @@
 	<?php $this->load->view('inc/header.php'); ?>
 	<div class="row">
 		<div class="span10">
-		<h2>Welcome to Attendum</h2>
-		<p>Attendum is a new way to reward students for engaging with their course.</p>
+			<h2>Welcome to Attendum</h2>
+			<p>Attendum is a new way to reward students for engaging with their course.</p>
+			<h3>Login</h3>
+			<form action="<?php echo site_url('user/login'); ?> method="post">
+			<label for="username">Username</label><input type="text" class="span3" name="username">
+			<label for="password">Password</label><input type="text" class="span3" name="password">
+			<input type="submit" name="login" class="btn large primary" value="Login">
+			</form>
 		</div>
 		<div class="span6">
 			<h3>Sign Up</h3>
@@ -24,7 +30,7 @@
 					<td><input type="password" name="passconf"></td>
 				</tr>
 				<tr>
-					<td><input type="submit" name="submit" value="Sign Up"></td>
+					<td><input type="submit" name="submit" class="btn large primary" value="Sign Up"></td>
 					<td></td>
 				</tr>
 				</table>
