6136b66365bc743dc795bf71309e712373cf5893
Will Pillar <me@willpillar.com>
2011-11-12 10:10:29 +0000

    Added a for loop.

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 session_start();
 $page_title = "Home"; //This will be the page title
 include("header.ini");
-echo "you suck harder blood!";
+echo "you suck harder!";
+
+for($i; $i<10; $i) {
+  echo "Sudo Team";
+}
+
 include("footer.ini");
 ?>
