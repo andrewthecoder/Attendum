a1e729ec0a80bb5ce1509d88a7ba82be5e7e8336
Andrew Beveridge <github@andrewbeveridge.co.uk>
2011-11-12 14:17:48 +0000

    new seed method

--- code.php
+++ code.php
@@ -1,7 +1,12 @@
 <?php
-$unid = 3;
+$cid=1;
+$start_time=1321121532;
+$end_time=1321122132;
+$lid=1;
+$seed = intval($cid+$start_time+$end_time+$lid);
+echo $seed;
 $chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWZYZ';
-mt_srand($unid);
+mt_srand($seed);
 for ($p = 0; $p < 6; $p++) {
 	$string .= $chars[mt_rand(0, 35)];
 }
