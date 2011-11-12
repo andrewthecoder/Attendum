be08d7615018fe25118453bb5dde8fe2f1b0e5e2
Harry George <monkeymanlives@gmail.com>
2011-11-12 13:51:07 +0000

    asd

--- application/views/statistics.php
+++ application/views/statistics.php
@@ -22,7 +22,7 @@
       data.addColumn('number', 'Slices');
       data.addRows([
         <?php foreach($user1 as $user):
-	  echo "['$user->email', $user->uid],;
+	  echo "['$user->email', $user->uid],
 	  "; 
 	  endforeach; ?>
         ['Pepperoni', 2]
