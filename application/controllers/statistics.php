2990dfcb12efa6541653185b1ec6f5413721b3d1
Harry George <monkeymanlives@gmail.com>
2011-11-12 12:49:25 +0000

    asd

--- application/controllers/statistics.php
+++ application/controllers/statistics.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Site extends CI_Controller {
+class statistics extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
