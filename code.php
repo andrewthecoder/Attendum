6a370cf75a86a52ab2ff5c75fc9c7465096ce288
Andrew Beveridge <github@andrewbeveridge.co.uk>
2011-11-12 13:50:49 +0000

    srand

--- code.php
+++ code.php
@@ -2,7 +2,7 @@
 $chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWZYZ';
 $string = 'LEC';
 for ($p = 0; $p < 6; $p++) {
-	$string .= $chars[mt_rand(0, 35)];
+	$string .= $chars[srand(0, 35)];
 }
 echo $string;
 ?>
