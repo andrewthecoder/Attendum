ae1c6f289f7fdb82652f3544a0631fd51e277187
Will Pillar <me@willpillar.com>
2011-11-12 14:24:48 +0000

    Changed some HTML.

--- application/views/home.php
+++ application/views/home.php
@@ -23,28 +23,28 @@ Points:<?php echo $a->points; ?>
 Description:<?php echo $a->description; ?>
 </p>
 <?php endforeach; ?>
-
-<h3>Sign Up</h3>
-<?php echo validation_errors('<div class="alert-message warning"><a class="close" href="#">×</a><p>','</p></div>'); ?>
-<form action="<?php site_url('user/signup'); ?>" method="post">
-<table>
-<tr>
-<td>Email</td>
-<td><input type="text" name="email"></td>
-</tr>
-<tr>
-<td>Password</td>
-<td><input type="password" name="pass"></td>
-<td>Confirm Password</td>
-<td><input type="password" name="passconf"></td>
-</tr>
-<tr>
-<td><input type="submit" name="submit" value="Sign Up"></td>
-<td></td>
-</tr>
-</table>
-</input>
-</form>
+	<div>
+		<h3>Sign Up</h3>
+		<?php echo validation_errors('<div class="alert-message warning"><a class="close" href="#">×</a><p>','</p></div>'); ?>
+		<form action="<?php site_url('user/signup'); ?>" method="post">
+			<table>
+			<tr>
+			<td>Email</td>
+			<td><input type="text" name="email"></td>
+			</tr>
+			<tr>
+			<td>Password</td>
+			<td><input type="password" name="pass"></td>
+			<td>Confirm Password</td>
+			<td><input type="password" name="passconf"></td>
+			</tr>
+			<tr>
+			<td><input type="submit" name="submit" value="Sign Up"></td>
+			<td></td>
+			</tr>
+			</table>
+		</form>
+	</div>
 </div>
 </body>
 </html>
