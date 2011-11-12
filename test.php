051ce395e2357f760102d2a8c18106277e4804f6
Harry George <monkeymanlives@gmail.com>
2011-11-12 09:49:33 +0000

    added header and footer file

--- test.php
+++ test.php
@@ -1,5 +1,8 @@
+<!--#include FILE="header.ini" -->
 <?php 
-
+session_start();
+$page_title = "Home"; //This will be the page title
+include("header.ini");
 echo "you suck harder!";
-
+include("footer.ini");
 ?>
