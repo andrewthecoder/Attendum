a1a8937d6a53887517d7348e3eef434e56c12c06
Ashley Heath <ashley.heath@live.com>
2011-11-12 12:58:54 +0000

    Just learning PHP

--- application/views/home.php
+++ application/views/home.php
@@ -7,7 +7,13 @@ Attendum - Rewarding Your Attendance
 <body>
 <div id="container">
 <?php foreach($user1 as $user): ?>
-<p><?php echo $user->email; ?></p>
+<p>
+<li><?php echo $user->uid; ?></li>
+<li><?php echo $user->email; ?></li>
+<li><?php echo $user->admin_rights; ?></li>
+<li><?php echo $user->unid; ?></li>
+<li><?php echo $user->password; ?></li>
+</p>
 <?php endforeach; ?>
 </div>
 </body>
