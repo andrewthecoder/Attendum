bf1b07221700fa81dd21c84a588605e708dd9f45
Ashley Heath <ashley.heath@live.com>
2011-11-12 23:27:11 +0000

    .....

--- application/views/comparison_chooser.php
+++ application/views/comparison_chooser.php
@@ -7,10 +7,6 @@ Achievements Comparison
 <body>
 <div id="container">
 <form action="<?php echo site_url('user/compare_achievements');?>" method="post">
-/*<p>Your email address</p>
-<input type="text" name="e1">
-<p>Someone else's email address</p>
-<input type="text" name="e2">*/
 <p>Enter the email of the person you want to compare achievements with.</p>
 <input type="text" name="e2">
 <input type="submit" value="Go">
