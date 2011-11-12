e13bad4c41c4bb6cf07d60e6fe99da09c8681d4b
Will Pillar <me@willpillar.com>
2011-11-12 11:00:49 +0000

    added view

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -19,7 +19,10 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$data = array (
+			'test' => 'hello world'
+		);
+		$this->load->view('welcome_message', $data);
 	}
 }
 
