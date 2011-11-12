32a65282a3fff7768eb30345448c9809621b1976
Will Pillar <me@willpillar.com>
2011-11-12 17:16:24 +0000

    Homepage HTML and styling changes.

--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -1,14 +1,19 @@
 <div id="header">
-<div class="span4">
-<img src="<?php echo base_url(); ?>images/logo.jpg" alt="Attendum" width="80px">
-</div>
-<div class="span12">
-<ul class="pills">
-	<li><a href="<?php echo site_url(); ?>">Home</a></li>
-	<li><a href="<?php echo site_url('site/about'); ?>">About</a></li>
-	<li><a href="<?php echo site_url('site/points'); ?>">Points</a></li>
-	<li><a href="<?php echo site_url('site/achievements'); ?>">Achievements</a></li>
-	<li><a href="<?php echo site_url('user/logout'); ?>">Logout</a></li>
-</ul>
-</div>
+	<div class="row">
+		<div class="span4">
+			<img src="<?php echo base_url(); ?>images/logo.jpg" alt="Attendum" width="80px">
+		</div>
+		<div class="span12">
+			<p id="logo_text">Attendum</a>
+		</div>
+	</div>
+	<div id="nav">
+		<ul class="pills">
+			<li><a href="<?php echo site_url(); ?>">Home</a> <span class="divider">/</span></li>
+			<li><a href="<?php echo site_url('site/about'); ?>">About</a> <span class="divider">/</span></li>
+			<li><a href="<?php echo site_url('site/points'); ?>">Points</a> <span class="divider">/</span></li>
+			<li><a href="<?php echo site_url('site/achievements'); ?>">Achievements</a> <span class="divider">/</span></li>
+			<li><a href="<?php echo site_url('user/logout'); ?>">Logout</a> <span class="divider">/</span></li>
+		</ul>
+	</div>
 </div>
