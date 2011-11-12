cba2f4704f974c409d28c22993bb3c6dc2f9aa52
Will Pillar <me@willpillar.com>
2011-11-12 14:59:25 +0000

    Fixed error validation message.

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -69,10 +69,10 @@ class User extends CI_Controller {
 	public function uni_check($str) {
 		$str_bits = explode('@',$str);
 		if($this->user_model->check_uni($str_bits[1])) {
-			$this->form_validation->set_message('uni_check', 'Your University is not currently registered with Attendum.');
 			return true;
 		}
 		else {
+			$this->form_validation->set_message('uni_check', 'Your University is not currently registered with Attendum.');
 			return false;
 		}
 		
