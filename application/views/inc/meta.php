11663b0e4bd8b19c84caac6fcad7fd790414cc1a
Will Pillar <me@willpillar.com>
2011-11-12 16:37:58 +0000

    Html changes to homepage. Added logo.

---
+++ application/views/inc/meta.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<head>
+<title>
+Attendum - Rewarding Your Attendance
+</title>
+<link rel="stylesheet" href="http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css">
+</head>
+<body>
