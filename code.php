c89866a67e8df83addf0342507c695c5b867c975
Andrew Beveridge <github@andrewbeveridge.co.uk>
2011-11-12 14:27:05 +0000

    grrrrr

--- code.php
+++ code.php
@@ -4,7 +4,8 @@ $start_time=1321121532;
 $end_time=1321122132;
 $lid=1;
 $seed = intval($cid+$start_time+$end_time+$lid);
-echo $seed + '<br /><br />';
+echo $seed;
+echo '<br /><br />';
 $chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWZYZ';
 mt_srand($seed);
 for ($p = 0; $p < 6; $p++) {
