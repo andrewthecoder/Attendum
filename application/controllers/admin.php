4f5b58827fb44b02193e3eae927b9490306f7e24
Andrew Beveridge <github@andrewbeveridge.co.uk>
2011-11-13 05:16:26 +0000

    fix timestamp hopefully

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -209,10 +209,12 @@ class Admin extends CI_Controller {
 	
 		$htmlrows = '';
 		foreach ($rows as $row) {
-			$bgcolor = 'FFB3B3';
 			if ($row->end_time > time()) {
 				$bgcolor = 'B3FFD7';
+			} elseif ($row->end_time < time()) {
+				$bgcolor = 'FFB3B3';
 			}
+			
 			$start_date = date('l jS \of F Y h:i A', $row->start_time);
 			$validity_unix = $row->end_time - $row->start_time;
 			$validity = (int)date('i', $validity_unix);
