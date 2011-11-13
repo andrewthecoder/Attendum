ea89765c06b73c0a84acd58ae7fad883e3ccf302
Will Pillar <me@willpillar.com>
2011-11-13 05:13:14 +0000

    fix

--- application/controllers/checkin.php
+++ application/controllers/checkin.php
@@ -51,9 +51,43 @@ class Checkin extends CI_Controller {
 					// insert usercode data
 					$this->usercode_model->insert_usercode($data);
 					
-					$this->load->model('user_achievement_model');
+					//$this->load->model('user_achievement_model');
 					
-					$achievement = $this->user_achievement_model->check_achievements($uid, $cid);
+					//$achievement = $this->user_achievement_model->check_achievements($uid, $cid);
+					
+					$query = $this->db->get('achievement');
+		
+					foreach($query->result() as $ach) {
+						$prep_sql = "SET @uid = ".$uid."; SET @cid = ".$cid.";";
+						$sql = $prep_sql.' '.$ach->sql.';';
+						
+						echo $sql;
+						
+						$CI =& get_instance();
+						$CI->load->model('module_model');
+						$mid = $CI->module_model->get_mid_cid($cid);
+						
+						$aid = $ach->aid;
+						
+						$query = $this->db->query($sql);
+						
+						$row = $query->row();
+						
+						echo 'MID: '.$mid;
+						echo 'AID: '.$aid;
+						echo 'CID: '.$cid;
+						
+						if($row->obtained == 1) {
+							$this->load->model('achievement_model');
+							
+							//award achievement
+							$this->achievement_model->award_achievement($uid, $aid, $mid);
+							return $this->achievement_model->get_achievement($aid);
+						}
+						else {
+							return false;
+						}
+					}
 					
 					// thank the muppets and redirect
 					$this->session->set_flashdata('checkin_success', 'Check-In Successful!');
