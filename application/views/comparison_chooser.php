d89f9ce9bdd429b7fd7aaec69115942cdabc9fec
Harry George <monkeymanlives@gmail.com>
2011-11-13 04:14:34 +0000

    bum bum bum buuuuuum!

--- application/views/comparison_chooser.php
+++ application/views/comparison_chooser.php
@@ -1,16 +1,11 @@
-<!doctype html>
-<head>
-<title>
-Achievements Comparison
-</title>
-</head>
-<body>
-<div id="container">
+<?php $this->load->view('inc/meta.php'); ?>
+<div class="container">
+	<?php $this->load->view('inc/header.php'); ?>
+	<div class="row">
 <form action="<?php echo site_url('user/compare_achievements');?>" method="post">
 <p>Enter the email of the person you want to compare achievements with.</p>
 <input type="text" name="e2">
 <input type="submit" value="Go">
 </form>
 </div>
-</body>
-</html>
+<?php $this->load->view('inc/footer.php'); ?>
