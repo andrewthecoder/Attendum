0671d6ef70ec189f890f65a079765e53690db538
Ashley Heath <ashley.heath@live.com>
2011-11-13 10:23:27 +0000

    nearly there

--- application/views/profile.php
+++ application/views/profile.php
@@ -49,15 +49,13 @@
 				
 		
 		<h3>Compare Achievements</h3>
-			<?php $this->load->view('inc/meta.php'); ?>
 			<div class="container">
-				<?php $this->load->view('inc/header.php'); ?>
 			<form action="<?php echo site_url('user/compare_achievements');?>" method="post">
 			<p>Enter the email of the person you want to compare achievements with.</p>
 			<input type="text" name="e2">
 			<input type="submit" value="Go">
 			</form>
-			<?php $this->load->view('inc/footer.php'); ?>
+
 		</div>
 		<div class="span8">
 			
