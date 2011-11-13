26a90bfe98d1e453104f8948bf25165d7d7b2c0f
Harry George <monkeymanlives@gmail.com>
2011-11-13 08:47:33 +0000

    test

--- application/views/profile.php
+++ application/views/profile.php
@@ -51,7 +51,7 @@
 	<h3>Currenct Achievements</h3>
 	<?php 
 	foreach($achievementStrings as $a):
-		echo $a->name.' - '.$a->points;
+		echo $a->name.' - '.$a->points.'<br>'; 
 	endforeach;
 	?>
 	<h3>Compare Achievements</h3>
