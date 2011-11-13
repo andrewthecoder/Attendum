f70f31b6b2f93653612a9c887563b5b35e332b26
Will Pillar <me@willpillar.com>
2011-11-13 02:17:58 +0000

    Adding admin backend.

---
+++ application/views/admin_create_reward.php
@@ -0,0 +1,7 @@
+<?php $this->load->view('inc/meta.php'); ?>
+<div class="container">
+	<?php $this->load->view('inc/header.php'); ?>
+	<h2>Create Reward</h2>
+	<?php $this->load->view('inc/admin_nav'); ?>
+</div>
+<?php $this->load->view('inc/footer.php'); ?>
--- application/views/inc/admin_nav.php
+++ application/views/inc/admin_nav.php
@@ -6,9 +6,11 @@
 		<li><a href="<?php echo site_url('/admin/create_code'); ?>">Create Code</a></li>
 		<li><a href="<?php echo site_url('/admin/create_module'); ?>">Create Module</a></li>
 		<li><a href="<?php echo site_url('/admin/list_codes'); ?>">View Codes</a></li>
+		<li><a href="<?php echo site_url('/admin/assign_award'); ?>">Assign Award</a></li>
 	<?php endif; ?>
 	<?php if($admin_rights >= 2): ?>
 		<li><a href="<?php echo site_url('/statistics/'); ?>">Statistics</a></li>
+		<li><a href="<?php echo site_url('/admin/create_reward'); ?>">Create Reward</a></li>
 	<?php endif; ?>
 	<?php if($admin_rights >= 3): ?>
 	<?php endif; ?>
