bc9b47bd5186a752fc3b73e5b71f2f62e1e2aeb8
Andrew Beveridge <github@andrewbeveridge.co.uk>
2011-11-13 09:28:21 +0000

    oops, explain! lol

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -302,7 +302,7 @@ class User extends CI_Controller {
 		$unid = $this->session->userdata['unid'];
 	
 		$query = $this->db->query("
-			EXPLAIN SELECT ((COUNT(DISTINCT c.cid) * 10) + (SUM(a.points))) AS points, u.uid AS uid, u.email AS email
+			SELECT ((COUNT(DISTINCT c.cid) * 10) + (SUM(a.points))) AS points, u.uid AS uid, u.email AS email
 			FROM 
 			code AS c 
 			LEFT JOIN usercode AS uc ON c.cid = uc.cid
