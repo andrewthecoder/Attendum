e9d98e70ab3e65808bf79bb7900cac8ffc778423
Ashley Heath <ashley.heath@live.com>
2011-11-13 11:37:20 +0000

    .

--- application/views/comparing_achievements.php
+++ application/views/comparing_achievements.php
@@ -26,14 +26,16 @@ else
 	echo $yourID;
 
 	//$this->load->database('userachievementmodule');
-	$query = $this->db->query('SELECT * FROM userachievementmodule WHERE uid='.$theirID);
+	$query = $this->db->query('SELECT a.name AS name, a.description AS description 
+	FROM userachievementmodule AS uam LEFT JOIN achievement AS a ON a.aid = uam.aid WHERE uid='.$theirID);
 	$theirAchievements = $query->result();
 	//foreach($query->result() as $row):
 	//	if($row->uid == $theirID){ $theirAchievements[] = $row->aid; }
 	//endforeach;
 	echo is_numeric($theirID);
 	echo is_numeric($yourID);
-	$query1 = $this->db->query('SELECT * FROM userachievementmodule WHERE uid='.$yourID);
+	$query1 = $this->db->query('SELECT a.name AS name, a.description AS description 
+	FROM userachievementmodule AS uam LEFT JOIN achievement AS a ON a.aid = uam.aid WHERE uid='.$yourID);
 	$yourAchievements = $query1->result();
 	//foreach($query->result() as $row):
 	//	if($row->uid == $yourID){ $yourAchievements[] = $row->aid; }
