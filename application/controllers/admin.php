42eb1d862fb2512d884236aefe7943782f8234e6
Will Pillar <me@willpillar.com>
2011-11-13 02:56:04 +0000

    fix spelling.

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -24,7 +24,7 @@ class Admin extends CI_Controller {
 	}
 	
 	public function assign_reward() {
-		this->load->model('module_model');
+		$this->load->model('module_model');
 		$this->load->helper('form');
 		$module_rows = $this->module_model->get_modules($this->session->userdata('uid'));
 		if($module_rows) {		
