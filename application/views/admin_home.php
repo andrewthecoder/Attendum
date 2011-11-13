1c53aff77736d6293c8da3ffcec0aa0bb8b24db4
Will Pillar <me@willpillar.com>
2011-11-13 01:52:36 +0000

    Admin nav.

--- application/views/admin_home.php
+++ application/views/admin_home.php
@@ -6,12 +6,12 @@
 	?>
 	<ul class="pills">
 		<?php if($admin_rights >= 1): ?>
-			<li><a href="#">Create Code</a></li>
-			<li><a href="#">Create Module</a></li>
-			<li><a href="#">View Codes</a></li>
+			<li><a href="<?php echo site_url('/admin/create_code'); ?>">Create Code</a></li>
+			<li><a href="<?php echo site_url('/admin/create_module'); ?>">Create Module</a></li>
+			<li><a href="<?php echo site_url('/admin/list_codes'); ?>">View Codes</a></li>
 		<?php endif; ?>
 		<?php if($admin_rights >= 2): ?>
-			<li><a href="#">Statistics</a></li>
+			<li><a href="<?php echo site_url('/statistics/'); ?>">Statistics</a></li>
 		<?php endif; ?>
 		<?php if($admin_rights >= 3): ?>
 		<?php endif; ?>
