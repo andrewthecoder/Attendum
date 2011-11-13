5a6198d470603b4c62c4ec42815cf2921d5e6276
Andrew Beveridge <github@andrewbeveridge.co.uk>
2011-11-13 09:31:06 +0000

    fix league

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -314,10 +314,7 @@ class User extends CI_Controller {
 			GROUP BY email
 			ORDER BY points DESC
 			LIMIT 0,30;");		
-		$leagueboard = $query->result();
-		
-		print_r($leagueboard);
-		die();
+		$leagueboard = $query->result_array();
 		
 		$htmlout = '<tr><td>Points</td><td>Username</td></tr>';
 		foreach ($leagueboard as $league_entry) {
