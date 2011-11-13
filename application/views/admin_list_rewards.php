5d1e2d3cf9adeb5c10744817c4aeb07a413fc80b
Ashley Heath <ashley.heath@live.com>
2011-11-13 04:09:48 +0000

    add

---
+++ application/views/admin_list_rewards.php
@@ -0,0 +1,17 @@
+<?php $this->load->view('inc/meta.php'); ?>
+<div class="container">
+	<?php $this->load->view('inc/header.php'); ?>
+	<h2>Admin Dashboard</h2>
+	<?php $this->load->view('inc/admin_nav'); ?>
+	<h2>Current Rewards</h2>
+	<?php 
+	$yourUniID = $this->session->userdata['unid'];
+	$rewards = $this->db->query("SELECT name, description FROM reward WHERE unid =$yourUniID");
+	foreach($rewards as $r):
+		$row = $r->row();
+		echo $row->name;
+		echo $row->description;
+	endfor;
+	?>
+</div>
+<?php $this->load->view('inc/footer.php'); ?>
