5bf90b828bd1ce08c217456dc549d7cb024d7831
Will Pillar <me@willpillar.com>
2011-11-13 01:43:54 +0000

    Changed Admin nav.

--- application/views/admin_home.php
+++ application/views/admin_home.php
@@ -5,15 +5,15 @@
 		$admin_rights = $this->session->userdata('admin_rights');
 	?>
 	<ul class="pills">
-		<?php if($admin_rights == 1): ?>
+		<?php if($admin_rights >= 1): ?>
 			<li><a href="#"></a>Create Code</li>
 			<li><a href="#"></a>Create Module</li>
 			<li><a href="#"></a>View Codes</li>
 		<?php endif; ?>
-		<?php if($admin_rights == 2): ?>
+		<?php if($admin_rights >= 2): ?>
 			<li><a href="#"></a>Statistics</li>
 		<?php endif; ?>
-		<?php if($admin_rights == 3): ?>
+		<?php if($admin_rights >= 3): ?>
 		<?php endif; ?>
 	</ul>
 </div>
