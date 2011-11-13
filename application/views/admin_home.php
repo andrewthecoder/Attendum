c946e375eeff079e34739c9f49e8e26a4dcb220a
Will Pillar <me@willpillar.com>
2011-11-13 01:41:54 +0000

    Added admin_home.php

---
+++ application/views/admin_home.php
@@ -0,0 +1,20 @@
+<?php $this->load->view('inc/meta.php'); ?>
+<div class="container">
+	<?php $this->load->view('inc/header.php'); ?>
+	<?php
+		$admin_rights = $this->session->userdata('admin_rights');
+	?>
+	<ul class="pills">
+		<?php if($admin_rights == 1): ?>
+			<li><a href="#"></a>Create Code</li>
+			<li><a href="#"></a>Create Module</li>
+			<li><a href="#"></a>View Codes</li>
+		<?php endif; ?>
+		<?php if($admin_rights == 2): ?>
+			<li><a href="#"></a>Statistics</li>
+		<?php endif; ?>
+		<?php if($admin_rights == 3): ?>
+		<?php endif; ?>
+	</ul>
+</div>
+<?php $this->load->view('inc/footer.php'); ?>
