dd1f6de2abe94c2169be35f6abd6dee67109b33b
Andrew Beveridge <github@andrewbeveridge.co.uk>
2011-11-13 07:31:28 +0000

    test

--- application/controllers/checkin.php
+++ application/controllers/checkin.php
@@ -76,10 +76,10 @@ class Checkin extends CI_Controller {
 						
 						
 						$clean_ach_sql = preg_replace("|.*SET.+(SELECT.+)|sU","\\1",$test_ach_sql);
-						$clean_ach_sql = str_replace("@cid",$cid,$clean_ach_sql)
-						$clean_ach_sql = str_replace("@uid",$uid,$clean_ach_sql)
-						$clean_ach_sql = str_replace("@mid",$external_sql_vars['mid'],$clean_ach_sql)
-						$clean_ach_sql = str_replace("@start",$external_sql_vars['start'],$clean_ach_sql)
+						$clean_ach_sql = str_replace("@cid",$cid,$clean_ach_sql);
+						$clean_ach_sql = str_replace("@uid",$uid,$clean_ach_sql);
+						$clean_ach_sql = str_replace("@mid",$external_sql_vars['mid'],$clean_ach_sql);
+						$clean_ach_sql = str_replace("@start",$external_sql_vars['start'],$clean_ach_sql);
 						
 						$sql_to_execute = explode(";",$clean_ach_sql);
 						
